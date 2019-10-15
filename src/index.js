4776cfee352f0f29f403ecfaea4b2c83e16a1260
Jon Jandoc <jjandoc@elizabethwarren.com>
2019-10-15 14:47:52 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 /* eslint-disable import/prefer-default-export */
 
 // Components
+export { default as Button } from './components/Button';
+export { default as Chevron } from './components/Chevron';
 export { default as HeroCard } from './components/HeroCard';
 
 // Hooks
