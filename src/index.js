57ab9d0bef7b72514887f57f8785b0fcbc0093c5
Jon Jandoc <jjandoc@elizabethwarren.com>
2019-10-15 14:48:58 -0700

    Update export (#3)
    * Update export
    
    * Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 /* eslint-disable import/prefer-default-export */
 
 // Components
+export { default as Button } from './components/Button';
+export { default as Chevron } from './components/Chevron';
 export { default as HeroCard } from './components/HeroCard';
 
 // Hooks
 
 // Utility functions
-export { default as createBreakpointStyles } from './utils/createBreakpointStyles';
 export { default as getContentfulImage } from './utils/getContentfulImage';
 export { default as serializeObject } from './utils/serializeObject';
