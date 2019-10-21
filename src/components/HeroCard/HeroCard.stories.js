25c93ccd16e25bed7caa8f2168da91b42357f957
Jon Jandoc <jjandoc@elizabethwarren.com>
2019-10-21 15:28:27 -0700

    Allow child components to be passed in to hero card

--- src/components/HeroCard/HeroCard.stories.js
+++ src/components/HeroCard/HeroCard.stories.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { text } from '@storybook/addon-knobs';
 import HeroCard from './index';
+import BodyCopy from '../BodyCopy';
+import Button, { ButtonLevels } from '../Button';
+import Headline, { HeadlineSizes } from '../Headline';
 
 export default {
   title: 'Components/HeroCard',
@@ -13,4 +16,17 @@ export const withContent = () => {
   const subtitle = text('Subtitle', 'We are happy to have you in this fight. Thank you for committing to vote for Elizabeth in the New Hampshire primary!');
   const image = text('Image', '//images.ctfassets.net/4ubxbgy9463z/3lPcBr8shu5sKGj0i2ZDRd/54437e08b70b346b42fbfbf2c1170dfd/new-hampshire.jpg');
   return (<HeroCard title={title} subtitle={subtitle} image={image} />);
+};
+
+export const withComponentChildren = () => {
+  const image = text('Image', '//images.ctfassets.net/4ubxbgy9463z/3lPcBr8shu5sKGj0i2ZDRd/54437e08b70b346b42fbfbf2c1170dfd/new-hampshire.jpg');
+  return (
+    <HeroCard image={image}>
+      <BodyCopy as="div">Here is some body copy</BodyCopy>
+      <Headline size={HeadlineSizes.XL} as="div">With a big title</Headline>
+      <p>
+        <Button level={ButtonLevels.WHITE}>Some CTA</Button>
+      </p>
+    </HeroCard>
+  );
 };
